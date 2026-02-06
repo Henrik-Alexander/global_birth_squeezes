54f88c393a308e40df3971865c9edaede324c96e
Schubert <schubert@demogr.mpg.de>
2026-02-06 12:21:33 +0100

    Update the reviewer comments

--- code/06_reviewer_comments.R
+++ code/06_reviewer_comments.R
@@ -53,13 +53,12 @@ ggplot(data = tfr_ratio, aes(x=tfr_ratio_1990s, y = `Male Childlessness`/`Female
   geom_hline(yintercept = 1, linetype = "dashed") +
   geom_vline(xintercept = 1, linetype = "dashed") +
   geom_point(aes(colour=Country)) +
-  geom_text(aes(label=Country, colour=Country), vjust=-0.8, family = "serif", size=3) +
+  geom_text(aes(label=Country, colour=Country), vjust=-0.8, family = "serif", size=2) +
   scale_x_log10("TFR ratio (1990s)", n.breaks=10) +
   scale_y_log10("Childlessness ratio (2010)", n.breaks=10) +
   scale_colour_viridis_d() +
   guides(colour = "none")
 ggsave(filename = "results/childlessness_tfr_ratio.pdf", height=10, width=14, unit="cm")
-
 
 ## Comment R. 1 C. 3 -----------------------------------------------------------
 
@@ -124,7 +123,6 @@ ggplot(data = subset(df_long, region == "France"), aes(x = year)) +
 # Goal: Replot figure 3
 # Distinguish into the mortality and the SRB component
 
-
 # Load the data
 load("data/decomp_pop.Rda")
 
@@ -132,6 +130,9 @@ load("data/decomp_pop.Rda")
 ref_year <- 1950
 ages <- seq(20, 50, by = 15)
 years <- seq(1960, 2100, by = 10)
+
+# Country selection
+country_selection <- c("China", "India", "Guatemala", "Rwanda", "Sub-Saharan Africa")
 
 ## Estimate the component
 
@@ -155,21 +156,9 @@ decomp_ref[, year:=NULL]
 decomp_long <- merge(decomp_long, decomp_ref, by=c("region", "age", "variable"), suffixes = c("", "_ref"), all.x=TRUE, all.y=FALSE, allow.cartesian=TRUE)
 
 # Select the data
-pop_ratio <- subset(decomp_long, year %in% years & age %in% ages & region %in% countries_examples & variable == "ratio_pop")
-decomp_long <- subset(decomp_long, year %in% years & age %in% ages & region %in% countries_examples & variable != "ratio_pop")
+pop_ratio <- subset(decomp_long, year %in% years & age %in% ages & region %in% country_selection & variable == "ratio_pop")
+decomp_long <- subset(decomp_long, year %in% years & age %in% ages & region %in% country_selection & variable != "ratio_pop")
 decomp_long[, variable:=str_to_title(str_replace(variable, "ratio_", "Contribution "))]
-
-# Plot the data longitudinally
-ggplot(data=decomp_long, aes(x=age, fill=variable, group=variable)) +
-  geom_hline(yintercept = 1) +
-  geom_col(aes(y=value), position=position_dodge()) +
-  geom_point(data=pop_ratio, aes(y=value)) +
-  #geom_hline(data=decomp_long, aes(yintercept=value, colour=variable, group=variable), linewidth=1.3) +
-  facet_grid(region ~ age) +
-  scale_fill_viridis_d() +
-  scale_colour_viridis_d() +
-  scale_y_log10(n.breaks=10) +
-  theme(panel.spacing.x=unit(1, "cm"))
 
 # Plot the final version of the figure
 ggplot(data=decomp_long, aes(x=year)) +
@@ -185,7 +174,7 @@ ggplot(data=decomp_long, aes(x=year)) +
   scale_y_log10("Sex Ratio", n.breaks=8) +
   theme(panel.spacing.x=unit(0.1, "cm"),
         legend.title = element_blank())
-ggsave(filename="results/plot_decomp_sex_ratio_contribution.pdf", height=20, width=20, unit="cm")
+ggsave(filename="results/plot_decomp_sex_ratio_contribution.pdf", height=25, width=20, unit="cm")
 
 ## Comment R. 1. C. 5 -----------------------------------------------------------
 
@@ -303,7 +292,26 @@ ggsave(filename = "results/tfr_ratio_age_childbearing.pdf", height=12, width=15,
 
 ## Comment R. 2 C. 2 ----------------------------------------------------------- 
 
-# Plot the gender gap in mortality
+
+## Overall gender balance
+wpp_pop <- read_xlsx("raw/WPP2024_GEN_F01_DEMOGRAPHIC_INDICATORS_COMPACT.xlsx", skip=16)
+wpp_pop <- as.data.table(wpp_pop)
+wpp_pop <- wpp_pop[, .(country = `Region, subregion, country or area *`,
+                       year = as.numeric(Year),
+                       pop_male = as.numeric(`Male Population, as of 1 July (thousands)`),
+                       pop_female = as.numeric(`Female Population, as of 1 July (thousands)`))]
+
+# Filter the non-missing values
+wpp_pop <- wpp_pop[!is.na(year) & !is.na(pop_male), ]
+
+
+# Estimate the gender balance globally
+wpp_pop[country == "World", .(sex_ratio = pop_male / pop_female), by = year] |> 
+  ggplot(aes(x=year, y=sex_ratio)) +
+  geom_line()
+
+
+# Load the life tables
 lt_future_f <- fread("raw/WPP2024_Life_Table_Complete_Medium_Female_2024-2100.csv")
 lt_future_m <- fread("raw/WPP2024_Life_Table_Complete_Medium_Male_2024-2100.csv")
 
@@ -340,7 +348,7 @@ ggsave("results/pop_ratio_ssa.pdf", width=15, height=10, unit="cm")
 load("data/wpp_male_tfr_prediction.Rda")
 
 # Estimate the ratio
-wpp_tfr_pop[, tfr_ratio := fit / tfr]
+wpp_tfr_pop[, tfr_ratio := (fit - tfr)/tfr]
 
 # Plot the results
 ggplot(subset(wpp_tfr_pop, variant_tfr=="Medium"), aes(x=year, y=tfr_ratio, group=year)) +
@@ -366,12 +374,12 @@ ggplot(tfr_distribution, aes(x=year)) +
   geom_line(aes(y=min_tfr_ratio, linetype = "Minimum")) +
   geom_ribbon(aes(ymin = lower80_tfr_ratio, ymax = upper80_tfr_ratio, alpha = "80%")) +
   geom_ribbon(aes(ymin = lower95_tfr_ratio, ymax = upper95_tfr_ratio, alpha = "95%")) +
-  geom_hline(yintercept = 1, colour = "red", linetype = "dotted") +
+  geom_hline(yintercept = 0, colour = "red", linetype = "dotted") +
   geom_vline(xintercept = 2025, colour = "red", linetype = "dotted") +
   scale_alpha_discrete("Quantiles: ", range = c(0.5, 0.3)) +
   scale_linetype_manual("", values = c(2, 1, 2)) +
   scale_x_continuous("Year", breaks = seq(1950, 2100, by = 10), expand = c(0, 0)) +
-  scale_y_log10("TFR ratio (male TFR / female TFR)", n.breaks=12)
+  scale_y_continuous("TFR difference (male TFR - female TFR) / female TFR", n.breaks=12)
 ggsave("results/distribution_tfr_ratio_time.pdf", height=10, width = 15, unit="cm")
 
 ### END ########################################################################
